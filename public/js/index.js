a3d536aec87d415c75f332618a378b71371174f5
GiL <iamleeyounggil@gmail.com>
2018-05-03 12:30:00 -0400

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,20 @@
 var socket = io()
 
+function scrollToBottom() {
+    var messages = $('#messages')
+    var newMessage = messages.children('li:last-child')
+
+    var clientHeight = messages.prop('clientHeight')
+    var scrollTop = messages.prop('scrollTop')
+    var scrollHeight = messages.prop('scrollHeight')
+    var newMessageHeight = newMessage.innerHeight()
+    var lastMessageHeight = newMessage.prev().innerHeight()
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+       messages.scrollTop(scrollHeight)
+    }
+}
+
 socket.on('connect', () => {
     console.log('connected to server')    
 })
@@ -15,6 +30,7 @@ socket.on('newMessage', (message) => {
     li.text(`${message.from} ${formattedTime} : ${message.text}`)
 
     $('#messages').append(li)
+    scrollToBottom()
 })
 
 socket.on('newLocationMessage', function (message) {
@@ -26,6 +42,7 @@ socket.on('newLocationMessage', function (message) {
     a.attr('href', message.url)
     li.append(a)
     $('#messages').append(li)
+    scrollToBottom()
 })
 
 $('#message-form').on('submit', function (e) {
