67c8cb0a3da76e9a8d2214af6c7e15bcad3825dd
GiL <iamleeyounggil@gmail.com>
2018-05-03 14:48:43 -0400

    Add users class and update userlist event

--- public/js/chat.js
+++ public/js/chat.js
@@ -16,7 +16,16 @@ function scrollToBottom() {
 }
 
 socket.on('connect', () => {
-    console.log('connected to server')    
+    var params = $.deparam(window.location.search)  
+
+    socket.emit('join', params, function (err) {
+        if(err) {
+            alert(err)
+            window.location.href= '/'
+        }else {
+            console.log('No error')
+        }
+    })
 })
 
 socket.on('disconnect', () => {
