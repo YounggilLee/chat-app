450bb2c83f22f2153fab1c234f50ce896effc5c1
GiL <iamleeyounggil@gmail.com>
2018-05-03 11:22:32 -0400

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -28,13 +28,15 @@ socket.on('newLocationMessage', function (message) {
 
 $('#message-form').on('submit', function (e) {
     e.preventDefault();
-    
+
+    var messageTextbox = $('[name=message]')
+
     socket.emit('createMessage', {
         from : 'User',
-        text: $('[name=message]').val()
+        text: messageTextbox.val()
     }, function() {
-
-    })
+        messageTextbox.val('')
+    })  
 })
 
 var locationButton = $('#send-location')
@@ -43,12 +45,16 @@ locationButton.on('click', function () {
         return alert('Geoloction not supported by you browser')
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...')
+
     navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send location')
         socket.emit('createLoactionMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location')
         alert('Unable to fetch location')
     })
 })
