27e9d1404b81cf2be7f5944a2f19a2550ff93ffc
GiL <iamleeyounggil@gmail.com>
2018-05-03 10:34:00 -0400

    geolocation support via geolocation api(Google)

--- public/js/index.js
+++ public/js/index.js
@@ -16,6 +16,16 @@ socket.on('newMessage', (message) => {
     $('#messages').append(li)
 })
 
+socket.on('newLocationMessage', function (message) {
+    var li = $('<li></li>')
+    var a = $('<a target="_blank">My Current Location</a>')
+
+    li.text(`${message.from}`)
+    a.attr('href', message.url)
+    li.append(a)
+    $('#messages').append(li)
+})
+
 $('#message-form').on('submit', function (e) {
     e.preventDefault();
     
@@ -24,5 +34,21 @@ $('#message-form').on('submit', function (e) {
         text: $('[name=message]').val()
     }, function() {
 
+    })
+})
+
+var locationButton = $('#send-location')
+locationButton.on('click', function () {
+    if(!navigator.geolocation){
+        return alert('Geoloction not supported by you browser')
+    }
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        socket.emit('createLoactionMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        })
+    }, function () {
+        alert('Unable to fetch location')
     })
 })
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect')
 
-var {generateMessage} = require('./message')
+var { generateMessage, generateLocationMessage} = require('./message')
 
 describe('generateMessage', () => {
     it('should generate corrent message obj', () => {
@@ -11,5 +11,18 @@ describe('generateMessage', () => {
         expect(typeof message.createdAt).toEqual('number')
         expect(message).toMatchObject({from, text})
 
+    })
+})
+
+describe('generateMessage', () => {
+    it('should generate corrent message obj', () => {
+        var from = 'Gil'
+        var latitude = 15
+        var longitude = 19
+        var url = 'https://www.google.com/maps?q=15,19'
+        var message = generateLocationMessage(from, latitude, longitude)
+
+        expect(typeof message.createdAt).toEqual('number')
+        expect(message).toMatchObject({ from, url })
     })
 })
